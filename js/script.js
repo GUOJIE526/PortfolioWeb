0aba9eb8930dfee0b85f05a7eab35ab3050ff304
Guo Jie <hungkaojay@gmail.com>
2024-10-15 21:03:30 +0800

    刪除js

--- js/script.js
+++
@@ -1,9 +0,0 @@
-new WOW().init();
-
-var typed = new Typed(".text", {
-  strings: ["FullStack Engineer", "Frontend Designer", "BackEnd Development"],
-  typeSpeed: 100,
-  backSpeed: 100,
-  backDelay: 1000,
-  loop: true,
-});
